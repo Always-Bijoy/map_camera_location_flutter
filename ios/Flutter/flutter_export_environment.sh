5ed4f6c6cb2c7b6d42e99189f45ffda15d900033
Bijoy Ghosh <bijoy@bitmascot.com>
2024-04-05 09:15:26 +0600

    flutter upgrade

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/bijoy/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/bijoy/Documents/personal/map_camera_flutter"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=G:\packages\map_camera_location_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=0.0.4"
+export "FLUTTER_BUILD_NUMBER=0.0.4"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
